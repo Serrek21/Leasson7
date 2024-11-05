ec6caf4801fe92baf13fe18c411c00ef5943c147
Serek <Serek@DESKTOP-3J8FKQN>
2024-11-05 20:55:55 +0200

    Добавьте файлы проекта.

---
+++ Leasson7/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Leasson7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EB = new System.Windows.Forms.Button();
+            this.rbEscape = new System.Windows.Forms.RadioButton();
+            this.rbFollow = new System.Windows.Forms.RadioButton();
+            this.rbDrag = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // EB
+            // 
+            this.EB.Location = new System.Drawing.Point(289, 153);
+            this.EB.Name = "EB";
+            this.EB.Size = new System.Drawing.Size(75, 34);
+            this.EB.TabIndex = 0;
+            this.EB.Text = "Catch me!";
+            this.EB.UseVisualStyleBackColor = true;
+            this.EB.Click += new System.EventHandler(this.escapeButton_Click);
+            // 
+            // rbEscape
+            // 
+            this.rbEscape.AutoSize = true;
+            this.rbEscape.Location = new System.Drawing.Point(58, 46);
+            this.rbEscape.Name = "rbEscape";
+            this.rbEscape.Size = new System.Drawing.Size(61, 17);
+            this.rbEscape.TabIndex = 1;
+            this.rbEscape.TabStop = true;
+            this.rbEscape.Text = "Escape";
+            this.rbEscape.UseVisualStyleBackColor = true;
+            this.rbEscape.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // rbFollow
+            // 
+            this.rbFollow.AutoSize = true;
+            this.rbFollow.Location = new System.Drawing.Point(166, 46);
+            this.rbFollow.Name = "rbFollow";
+            this.rbFollow.Size = new System.Drawing.Size(55, 17);
+            this.rbFollow.TabIndex = 2;
+            this.rbFollow.TabStop = true;
+            this.rbFollow.Text = "Follow";
+            this.rbFollow.UseVisualStyleBackColor = true;
+            // 
+            // rbDrag
+            // 
+            this.rbDrag.AutoSize = true;
+            this.rbDrag.Location = new System.Drawing.Point(260, 46);
+            this.rbDrag.Name = "rbDrag";
+            this.rbDrag.Size = new System.Drawing.Size(48, 17);
+            this.rbDrag.TabIndex = 3;
+            this.rbDrag.TabStop = true;
+            this.rbDrag.Text = "Drag";
+            this.rbDrag.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rbDrag);
+            this.Controls.Add(this.rbFollow);
+            this.Controls.Add(this.rbEscape);
+            this.Controls.Add(this.EB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button EB;
+        private System.Windows.Forms.RadioButton rbEscape;
+        private System.Windows.Forms.RadioButton rbFollow;
+        private System.Windows.Forms.RadioButton rbDrag;
+    }
+}
+
---
+++ Leasson7/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Leasson7
+{
+    public partial class Form1 : Form
+    {
+        private const int escapeD = 30;
+        private bool isDragging = false;
+        private Point dragOffset;
+        public Form1()
+        {
+            InitializeComponent();
+            this.MouseMove += MainForm_MouseMove;
+            EB.MouseDown += EB_MouseDown;
+            EB.MouseMove += EB_MouseMove;
+            EB.MouseUp += EB_MouseUp;
+        }
+
+        private void MainForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (rbEscape.Checked)
+            {
+                EscapeFromMouse(e);
+            }
+            else if (rbFollow.Checked)
+            {
+                FollowMouse(e);
+            }
+        }
+
+        private void EscapeFromMouse(MouseEventArgs e)
+        {
+            var bCenter = new Point(
+                 EB.Left + EB.Width / 2,
+                 EB.Top + EB.Height / 2
+            );
+
+            var X = bCenter.X - e.X;
+            var Y = bCenter.Y - e.Y;
+            var d = Math.Sqrt(X * X + Y * Y);
+
+            if (d < escapeD * 2)
+            {
+                var setX = escapeD * X / d;
+                var setY = escapeD * Y / d;
+
+                var newBL = new Point(
+                    (int)(EB.Left + setX),
+                    (int)(EB.Top + setY)
+                );
+
+                if (newBL.X >= 0 && newBL.X + EB.Width <= ClientSize.Width &&
+                    newBL.Y >= 0 && newBL.Y + EB.Height <= ClientSize.Height)
+                {
+                    EB.Location = newBL;
+                }
+            }
+        }
+        private void FollowMouse(MouseEventArgs e)
+        {
+            var newBL1 = new Point(
+                e.X - EB.Width / 2,
+                e.Y - EB.Height / 2
+            );
+
+            if (newBL1.X >= 0 && newBL1.X + EB.Width <= ClientSize.Width &&
+                newBL1.Y >= 0 && newBL1.Y + EB.Height <= ClientSize.Height)
+            {
+                EB.Location = newBL1;
+            }
+        }
+        private void EB_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (rbDrag.Checked && e.Button == MouseButtons.Left)
+            {
+                isDragging = true;
+                dragOffset = new Point(e.X, e.Y);
+            }
+        }
+        private void EB_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                var newL = new Point(
+                    EB.Left + e.X - dragOffset.X,
+                    EB.Top + e.Y - dragOffset.Y
+                );
+
+                if (newL.X >= 0 && newL.X + EB.Width <= ClientSize.Width &&
+                    newL.Y >= 0 && newL.Y + EB.Height <= ClientSize.Height)
+                {
+                    EB.Location = newL;
+                }
+            }
+        }
+
+        private void EB_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (rbDrag.Checked && e.Button == MouseButtons.Left)
+            {
+                isDragging = false;
+            }
+        }
+
+        private void escapeButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
